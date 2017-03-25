7cca9848e080c81b99b0e1656187ff3cbe78ac49
Felipe Cavalcanti <fjfcavalcanti@gmail.com>
2017-03-24 23:17:17 -0300

    initial commit

--- main.go
+++ main.go
@@ -1,40 +1,30 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/garyburd/redigo/redis"
-	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"sync"
+
+	"github.com/garyburd/redigo/redis"
+	"github.com/gorilla/websocket"
+	"github.com/serialx/hashring"
 )
 
-/*
-   route: 'player.message',
-   args: {
-     playerId: randomUUID,
-     msg: 'andre presunto',
-   },
-*/
-
 var upgrader = websocket.Upgrader{}
-var redisPubSubClient redis.Conn
-var redisPubSub *redis.PubSubConn
+var redisClients map[string]redis.Conn
+var redisPubSubClients map[string]*redis.PubSubConn
 var playerToSocketMap map[string]*websocket.Conn
-var redisClient redis.Conn
 var lock sync.RWMutex
 var redisLock sync.RWMutex
 var redisPubSubLock sync.RWMutex
-
-type msg struct {
-	route string
-	args  map[string]string
-}
+var ring *hashring.HashRing
 
 func clearPlayer(playerID, channel string, c *websocket.Conn) {
 	redisPubSubLock.Lock()
+	redisPubSub := getRedisPubSubForPlayer(playerID)
 	redisPubSub.Unsubscribe(channel)
 	redisPubSubLock.Unlock()
 	c.Close()
@@ -44,18 +34,20 @@ func clearPlayer(playerID, channel string, c *websocket.Conn) {
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
-	userID := r.RequestURI[1:]
-	channel := fmt.Sprintf("%s.messages", userID)
+	playerID := r.RequestURI[1:]
+	channel := fmt.Sprintf("%s.messages", playerID)
 	redisPubSubLock.Lock()
+	redisPubSub := getRedisPubSubForPlayer(playerID)
 	redisPubSub.Subscribe(channel)
 	redisPubSubLock.Unlock()
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		return
 	}
-	defer clearPlayer(userID, channel, c)
+	defer clearPlayer(playerID, channel, c)
 	lock.Lock()
-	playerToSocketMap[userID] = c
+	log.Printf("the player id: %s", playerID)
+	playerToSocketMap[playerID] = c
 	lock.Unlock()
 
 	for {
@@ -64,6 +56,7 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 			break
 		}
 		redisLock.Lock()
+		redisClient := getRedisForPlayer(playerID)
 		_, err = redisClient.Do("PUBLISH", channel, message)
 		redisLock.Unlock()
 		if err != nil {
@@ -73,24 +66,34 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func receiveFromRedis() {
-	var err error
-	redisURL := os.Getenv("REDIS_URL")
-	redisPubSubClient, err = redis.DialURL(redisURL)
+func getRedisForPlayer(playerID string) redis.Conn {
+	server, _ := ring.GetNode(playerID)
+	return redisClients[server]
+}
+
+func getRedisPubSubForPlayer(playerID string) *redis.PubSubConn {
+	server, _ := ring.GetNode(playerID)
+	return redisPubSubClients[server]
+}
+
+func receiveFromRedis(redisURL string) {
+	redisPubSubClient, err := redis.DialURL(redisURL)
 	if err != nil {
 		log.Println(err.Error())
 	}
 	defer redisPubSubClient.Close()
-	redisPubSub = &redis.PubSubConn{Conn: redisPubSubClient}
+	redisPubSub := &redis.PubSubConn{Conn: redisPubSubClient}
+	redisPubSubClients[redisURL] = redisPubSub
 	for {
 		switch v := redisPubSub.Receive().(type) {
 		case redis.Message:
-			var playerMessage msg
-			json.Unmarshal(v.Data, &playerMessage)
-			enemyID := playerMessage.args["playerID"]
+			parts := strings.Split(string(v.Data), ",")
+			enemyID := parts[0]
 			lock.RLock()
 			if enemySocket, ok := playerToSocketMap[enemyID]; ok {
 				enemySocket.WriteMessage(websocket.TextMessage, v.Data)
+			} else {
+				// log.Printf("enemyId is %s\n", enemyID)
 			}
 			lock.RUnlock()
 		case error:
@@ -101,15 +104,21 @@ func receiveFromRedis() {
 
 func main() {
 	playerToSocketMap = map[string]*websocket.Conn{}
+	redisClients = map[string]redis.Conn{}
+	redisPubSubClients = map[string]*redis.PubSubConn{}
 	http.HandleFunc("/", wsHandler)
 	var err error
-	redisURL := os.Getenv("REDIS_URL")
-	redisClient, err = redis.DialURL(redisURL)
-	if err != nil {
-		log.Println(err.Error())
+	redisURLs := strings.Split(os.Getenv("REDIS_URL"), ",")
+	ring = hashring.New(redisURLs)
+	for _, redisURL := range redisURLs {
+		go receiveFromRedis(redisURL)
+		redisClient, err := redis.DialURL(redisURL)
+		if err != nil {
+			log.Println(err.Error())
+		}
+		redisClients[redisURL] = redisClient
+		defer redisClient.Close()
 	}
-	defer redisClient.Close()
-	go receiveFromRedis()
 	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(fmt.Sprintf("ListenAndServe: %s", err.Error()))
